4b4f3e2a9d6f669e55cce85fec7c6cda206c3507
Vikram Shrivastav <vikrams13122004@gmail.com>
2025-02-12 20:59:43 +0530

    Update main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@chakra-ui/react"
+
 function App() {
   return (
     <>
       <h1 className="text-bold text-xl font-mono">Frontend Template</h1>
-      
+      <Button>Hello</Button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {Provider} from "@/components/ui/provider"
+import {Provider} from "./components/ui/provider.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
